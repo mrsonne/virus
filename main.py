140305b2fa4d3440f11112b1efc4a47702083ffd
Jacob Sonne <jacob.sonne@nov.com>
2020-04-04 21:33:06 +0200

    add calls to contour plotter

--- main.py
+++ main.py
@@ -9,10 +9,14 @@ from src import countries
 
 # How many people do you meet per day
 encounters_per_day = 50 # adjusted to fit flu data (adjusted with transmission probability)
-# encounters_per_day = 5 # damped sick count
-# encounters_per_day = 8 # flattened
+# encounters_per_day = 15 # damped sick count
+# encounters_per_day = 35 # flattened
 
 # encounters_per_day = None # constant sick count
 
-countries.run_country('flu', 'denmark', encounters_per_day)
-# countries.run_country('covid19', 'denmark', encounters_per_day)
+# countries.run_country('flu', 'denmark', encounters_per_day)
+countries.run_country('covid19', 'denmark', encounters_per_day, show_recovered=True, tspan=[0,320])
+
+
+# countries.map_country('covid19', 'denmark', encounters_per_day, tspan=[0, 400])
+# countries.map_country('flu', 'denmark', encounters_per_day, tspan=[0, 400])
