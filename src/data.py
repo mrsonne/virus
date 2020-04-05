554f28a1733598986a1285c49fc11a89ad20d2cf
Jacob Sonne <jacob.sonne@nov.com>
2020-04-05 20:23:33 +0200

    use p_d = 0.5 % as stated in text

--- src/data.py
+++ src/data.py
@@ -2,11 +2,10 @@ def get_covid19_parameters():
     # Assumes same hospitalization rate as flu. Data is not reliable for COVID-19 yet. Parameter is not critical
     _, p_transmision, _, p_sick_to_hospitalized, p_sick_to_dead, _, _ = get_flu_parameters()
     tspan = [0, 200]
-    p_sick_to_dead *= 6. # relative to flu
+    p_sick_to_dead = .005 
     # p_sick_to_dead_no_ventilator =  p_sick_to_dead # For testing: no effect of exceeding ventilator capacity
 
     # https://medium.com/@tomaspueyo/coronavirus-the-hammer-and-the-dance-be9337092b56
-    # p_sick_to_dead_no_ventilator =  0.04 # TODO: Of all patients including collateral effects. Source??
     p_sick_to_dead_no_ventilator = .9 # 
     p_hospitalized_to_ventilator = 0.2 # of hospitalized
     tau = 14 #days
@@ -17,7 +16,7 @@ def get_flu_parameters():
     # Get flu data and use as baseline
     p_sick_to_hospitalized, p_sick_to_dead = get_us_flu_data() 
     tspan = [0, 700]
-    p_sick_to_dead_no_ventilator = 0. # TODO: of the patients requirering ventilator?
+    p_sick_to_dead_no_ventilator = 0.
     p_transmision = 0.0026 # adjusted to fit flu data
     p_hospitalized_to_ventilator = 0. # unknown
     tau = 7 #days
