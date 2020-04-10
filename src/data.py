401e9274f398249710877f1b64af80ff95254731
Jacob Sonne <jacob.sonne@nov.com>
2020-04-10 08:11:02 +0200

    use new names for parameters

--- src/data.py
+++ src/data.py
@@ -1,26 +1,26 @@
 def get_covid19_parameters():
     # Assumes same hospitalization rate as flu. Data is not reliable for COVID-19 yet. Parameter is not critical
-    _, p_transmision, _, p_sick_to_hospitalized, p_sick_to_dead, _, _ = get_flu_parameters()
+    _, p_t, _, p_h, p_d, _, _ = get_flu_parameters()
     tspan = [0, 200]
-    p_sick_to_dead = .005 
-    # p_sick_to_dead_no_ventilator =  p_sick_to_dead # For testing: no effect of exceeding ventilator capacity
+    p_d = .005 
+    # p_dnv =  p_d # For testing: no effect of exceeding ventilator capacity
 
     # https://medium.com/@tomaspueyo/coronavirus-the-hammer-and-the-dance-be9337092b56
-    p_sick_to_dead_no_ventilator = .9 # 
-    p_hospitalized_to_ventilator = 0.2 # of hospitalized
+    p_dnv = .9 # 
+    p_v = 0.2 # of hospitalized
     tau = 14 #days
-    return tspan, p_transmision, p_hospitalized_to_ventilator, p_sick_to_hospitalized, p_sick_to_dead, p_sick_to_dead_no_ventilator, tau
+    return tspan, p_t, p_v, p_h, p_d, p_dnv, tau
 
 
 def get_flu_parameters():
     # Get flu data and use as baseline
-    p_sick_to_hospitalized, p_sick_to_dead = get_us_flu_data() 
+    p_h, p_d = get_us_flu_data() 
     tspan = [0, 700]
-    p_sick_to_dead_no_ventilator = 0.
-    p_transmision = 0.0026 # adjusted to fit flu data
-    p_hospitalized_to_ventilator = 0. # unknown
+    p_dnv = 0.
+    p_t = 0.0026 # adjusted to fit flu data
+    p_v = 0. # unknown
     tau = 7 #days
-    return tspan, p_transmision, p_hospitalized_to_ventilator, p_sick_to_hospitalized, p_sick_to_dead, p_sick_to_dead_no_ventilator, tau
+    return tspan, p_t, p_v, p_h, p_d, p_dnv, tau
 
 
 def get_us_flu_data():
@@ -28,9 +28,9 @@ def get_us_flu_data():
     # https://www.cdc.gov/flu/about/burden/preliminary-in-season-estimates.htm
     # population = 250.e6
     cases = 45000000. # per year
-    p_sick_to_hospitalized = 600000. / cases # ~1.3 %
-    p_sick_to_dead = 40000. / cases # ~0.1 %
-    return p_sick_to_hospitalized, p_sick_to_dead
+    p_h = 600000. / cases # ~1.3 %
+    p_d = 40000. / cases # ~0.1 %
+    return p_h, p_d
 
 PARSFUN_FOR_VIRUSID = dict(flu=get_flu_parameters,
                            covid19=get_covid19_parameters)
